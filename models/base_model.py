1bdbbc6748e94eac4baf73a08909c7344702a4e0
Makarona10 <ahmedneyssi@gmail.com>
2024-03-06 18:44:26 +0200

    Base Model updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import datetime
+from datetime import datetime
 import uuid
 
 """ BaseModel module """
@@ -12,7 +12,7 @@ class BaseModel:
     def __init__(self):
         '''The initialization for BaseModel class'''
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
+        self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
@@ -20,12 +20,14 @@ class BaseModel:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        '''Update the date due the last modification of an object'''
-        self.updated_at = datetime.datetime.now().isoformat()
+        '''Update the date due to the last modification of an object'''
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         '''Returns a dictionary containing all keys/values
         of __dict__ of the instance'''
-        cls_name = {"__class__": self.__class__.__name__}
-        merge = {**cls_name, **self.__dict__}
-        return merge
+        new_dic = self.__dict__.copy
+        new_dic['__class__'] = self.__class__.__name__
+        new_dic['created_at'] = self.created_at.isoformat()
+        new_dic['updated_at'] = self.updated_at.isoformat()
+        return new_dic
