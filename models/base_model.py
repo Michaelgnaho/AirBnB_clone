6d5c2295dafde051b96665aaae0a92dc2efaa205
Makarona10 <ahmedneyssi@gmail.com>
2024-03-06 20:09:57 +0200

    Task 4 updated

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,24 @@ class BaseModel:
     '''The base class that defines all
     common attributes/methods for other classes'''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''The initialization for BaseModel class'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for k, val in kwargs.items():
+                if k == 'created_at':
+                    self.created_at = datetime.strptime(
+                        kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == "__class__":
+                    continue
+                else:
+                    setattr(self, k, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         '''Returns a representation of an object of BaseModel class'''
@@ -31,3 +44,6 @@ class BaseModel:
         new_dic['created_at'] = self.created_at.isoformat()
         new_dic['updated_at'] = self.updated_at.isoformat()
         return new_dic
+
+r = BaseModel(created_at="12/5/2022")
+print(r.id)
