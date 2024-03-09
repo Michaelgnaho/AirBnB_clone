bf353ed59277a9cb8a3347f0793ab6b9761acdc8
Makarona10 <ahmedneyssi@gmail.com>
2024-03-10 01:11:09 +0200

    s

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,49 +1,38 @@
 #!/usr/bin/python3
-"""FileStorage class defination."""
+""" FileStorage module """
+
 import json
+from models.review import Review
 from models.base_model import BaseModel
+from models.city import City
 from models.user import User
 from models.state import State
-from models.city import City
 from models.place import Place
 from models.amenity import Amenity
-from models.review import Review
-
 
 class FileStorage:
-    """storage engine repesentaion as abstract.
-
-    Attributes:
-        __file_path (str): file name to save the obj
-        __objects (dict): instantiated objects as a dict.
-    """
+    '''File storage class'''
     __file_path = "file.json"
     __objects = {}
-
+    
     def all(self):
-        """dictionary of __objects is return."""
+        '''Returns the dictionary __objects'''
         return FileStorage.__objects
-
-    def new(self, o):
-        """Set in __objects"""
-        ret = o.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ret, o.id)] = o
+    
+    def new(self, obj):
+        '''sets in __objects the obj with key <obj class name>.id'''
+        FileStorage.__objects[f"{__class__.__name__}.{self.id}"] = obj
 
     def save(self):
-        """Sequnce of __objects to the JSON file"""
-        o2 = FileStorage.__objects
-        o3 = {o: o2[o].to_dict() for o in o2.keys()}
-        with open(FileStorage.__file_path, "w") as fille:
-            json.dump(o3, fille)
-
+        '''serializes __objects to the JSON file'''
+        x = FileStorage.__objects
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(FileStorage.__objects, f)
+    
     def reload(self):
-        """No sequence of the  __file_path to __objects."""
+        '''deserializes the JSON file to __objects'''
         try:
-            with open(FileStorage.__file_path) as fille2:
-                o3 = json.load(fille2)
-                for obj in o3.values():
-                    class_name = obj["__class__"]
-                    del obj["__class__"]
-                    self.new(eval(class_name)(**obj))
+            with open(FileStorage.__file_path, 'r') as f:
+                FileStorage.__objects = json.load(f)
         except FileNotFoundError:
-            return
+            pass
