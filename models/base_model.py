632dde13c943f0c99bce04fc97c5024c1bd7b882
Makarona10 <ahmedneyssi@gmail.com>
2024-03-10 01:21:55 +0200

    fixes done

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,74 @@
 #!/usr/bin/python3
-from datetime import datetime
+"""Defines all common attributes/methods for other classes
+"""
 import uuid
+from datetime import datetime
 import models
-
-""" BaseModel module """
 
 
 class BaseModel:
-    '''The base class that defines all
-    common attributes/methods for other classes'''
+    """Base class for all models"""
 
     def __init__(self, *args, **kwargs):
-        '''The initialization for BaseModel class'''
+        """Initialization of a Base instance.
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
         if kwargs:
-            for k, val in kwargs.items():
-                if k == 'created_at':
-                    self.created_at = datetime.strptime(
-                        kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                elif k == 'updated_at':
-                    self.updated_at = datetime.strptime(
-                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                elif k == "__class__":
+            dtime_format = '%Y-%m-%dT%H:%M:%S.%f'
+            for key, value in kwargs.items():
+                if key == '__class__':
                     continue
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(
+                        kwargs['created_at'], dtime_format)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        kwargs['updated_at'], dtime_format)
                 else:
-                    setattr(self, k, val)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
+            self.updated_at = datetime.now()
+            models.storage.new(self)  # as instructed also in task 5
 
     def __str__(self):
-        '''Returns a representation of an object of BaseModel class'''
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """Returns a readable string representation
+        of BaseModel instances"""
+
+        clsName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
 
     def save(self):
-        '''Update the date due to the last modification of an object'''
-        models.storage.save(self)
+        """Updates the public instance attribute updated_at
+        with the current datetime"""
+
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        '''Returns a dictionary containing all keys/values
-        of __dict__ of the instance'''
-        new_dic = self.__dict__.copy()
-        new_dic['__class__'] = self.__class__.__name__
-        new_dic['created_at'] = self.created_at.isoformat()
-        new_dic['updated_at'] = self.updated_at.isoformat()
-        return new_dic
+        """Returns a dictionary that contains all
+        keys/values of the instance"""
+        my_dict = self.__dict__.copy()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['__class__'] = self.__class__.__name__
+        # OR
+        # my_dict = dict()
+        # my_dict['__class__'] = self.__class__.__name__
+        # for key, value in self.__dict__.items():
+        #    if key in ('created_at', 'updated_at'):
+        #        my_dict[key] = value.isoformat()
+        #    else:
+        #        my_dict[key] = value
+        return my_dict
+        # my_dict = dict()
+        # my_dict['__class__'] = self.__class__.__name__
+        # for key, value in self.__dict__.items():
+        #    if type(value) is datetime:
+        #        my_dict[key] = value.isoformat()
+        #    else:
+        #        my_dict[key] = value
+        # return my_dict
