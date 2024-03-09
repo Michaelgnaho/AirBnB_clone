7aa4a7af95e6b480b25ff236eee818636ed1b756
Makarona10 <ahmedneyssi@gmail.com>
2024-03-09 20:56:25 +0200

    s

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         '''Returns a representation of an object of BaseModel class'''
@@ -35,13 +36,14 @@ class BaseModel:
 
     def save(self):
         '''Update the date due to the last modification of an object'''
-        storage.save()
+        
+        storage.save(self)
         self.updated_at = datetime.now()
 
     def to_dict(self):
         '''Returns a dictionary containing all keys/values
         of __dict__ of the instance'''
-        new_dic = self.__dict__.copy
+        new_dic = self.__dict__.copy()
         new_dic['__class__'] = self.__class__.__name__
         new_dic['created_at'] = self.created_at.isoformat()
         new_dic['updated_at'] = self.updated_at.isoformat()
